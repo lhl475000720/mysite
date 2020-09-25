9e9cb2b6590effba30a1dd3e33185c4072a7fe8b
hllei@fitgreat.cn <hllei@fitgreat.cn>
2020-09-25 18:28:19 +0800

    Add User

--- init_env.sh
+++ init_env.sh
@@ -19,7 +19,7 @@ if [ ! -f $md5 ] ; then
 	createrequirementsmd5
   /root/anaconda3/bin/conda create -y python=3.7 --prefix=/root/anaconda3/envs/test2
 #  echo "end"
-  /root/anaconda3/bin/conda activate test2
+  source /root/anaconda3/bin/conda activate test2
 #  pip install -r requirements.txt
 #  /root/anaconda3/bin/conda deactivate
 #  bash ./init_docker.sh
