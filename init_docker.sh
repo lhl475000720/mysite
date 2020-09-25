0a1637e34182e37d96548fcb62db134c6a5f32bb
hllei@fitgreat.cn <hllei@fitgreat.cn>
2020-09-25 16:49:34 +0800

    Add User

---
+++ init_docker.sh
@@ -0,0 +1,31 @@
+image_file=/home/lei/app/test2/www/Dockerfile
+image_file_md5=image_file_md5_package
+
+image_file_md5_new=$(md5sum -b $image_file | awk '{print $1}'|sed 's/ //g')
+
+function createdockerfilemd5() {
+    sudo echo $image_file_md5_new > image_file_md5
+}
+
+function initcontainer(){
+  docker run -p 10010:80 --name=test2 -v /home/lei/app/test2/www/:/user/local/apache2/htdocs/ -v /home/lei/app/test2/conf/httpd.conf:/usr/local/apache2/conf/httpd.conf -v /home/lei/app/test2/logs/:/usr/local/apache2/logs/ -v /root/anaconda3/envs/test2:/usr/local/anaconda -d ubuntu:hhhh
+}
+
+if [ ! -f $image_file_md5 ] ; then
+  createdockerfilemd5
+  docker build -t ubuntu:hhhh .
+#  $image_name='ubuntu:hhhh'
+  initcontainer
+  exit
+fi
+
+image_file_md5_old=$(cat $image_file_md5|sed 's/ //g')
+if [ "$image_file_md5_new" == "$image_file_md5_old" ] ; then
+	docker restart test2
+else
+	docker stop test2
+	docker rm test2
+	docker build -t ubuntu:dddd .
+#	$image_name='ubuntu:dddd'
+	initcontainer
+fi
--- init_env.sh
+++ init_env.sh
@@ -2,24 +2,19 @@
 requirements=/home/lei/app/test2/www/requirements.txt
 md5=package_md5
 
-image_file=/home/lei/app/test2/www/Dockerfile
-image_file_md5=image_file_md5_package
+
 
 requirements_md5_new=$(md5sum -b $requirements | awk '{print $1}'|sed 's/ //g')
-image_file_md5_new=$(md5sum -b $image_file | awk '{print $1}'|sed 's/ //g')
+
 
 
 function createrequirementsmd5(){
      sudo echo $requirements_md5_new > $md5
 }
 
-function createdockerfilemd5() {
-    sudo echo $image_file_md5_new > image_file_md5
-}
 
-function initcontainer(){
-  docker run -p 10010:80 --name=test2 -v /home/lei/app/test2/www/:/user/local/apache2/htdocs/ -v /home/lei/app/test2/conf/httpd.conf:/usr/local/apache2/conf/httpd.conf -v /home/lei/app/test2/logs/:/usr/local/apache2/logs/ -v /root/anaconda3/envs/test2:/usr/local/anaconda -d ubuntu:hhhh
-}
+
+
 
 if [ ! -f $md5 ] ; then
 	createrequirementsmd5
@@ -27,33 +22,22 @@ if [ ! -f $md5 ] ; then
   conda activate test2
   pip install -r requirements.txt
   conda deactivate
+  bash ./init_docker.sh
+  exit
 fi
 
-if [ ! -f $image_file_md5 ] ; then
-  createdockerfilemd5
-  docker build -t ubuntu:hhhh .
-#  $image_name='ubuntu:hhhh'
-  initcontainer
-fi
+
 
 requirements_md5_old=$(cat $md5|sed 's/ //g')
 if [ "$requirements_md5_new" == "$requirements_md5_old" ] ; then
 	echo "not change"
+	bash ./init_docker.sh
 else
 	createrequirementsmd5
 	conda activate test2
   pip install -r requirements.txt
   conda deactivate
+  bash ./init_docker.sh
 fi
 
 
-image_file_md5_old=$(cat $image_file_md5|sed 's/ //g')
-if [ "$image_file_md5_new" == "$image_file_md5_old" ] ; then
-	docker restart test2
-else
-	docker stop test2
-	docker rm test2
-	docker build -t ubuntu:dddd .
-#	$image_name='ubuntu:dddd'
-	initcontainer
-fi
