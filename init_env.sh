233a38d836330f3c42af6a0d312f2c5432a86f44
hllei@fitgreat.cn <hllei@fitgreat.cn>
2020-09-25 18:45:46 +0800

    Add User

--- init_env.sh
+++ init_env.sh
@@ -17,7 +17,7 @@ function createrequirementsmd5(){
 
 if [ ! -f $md5 ] ; then
 	createrequirementsmd5
-#  /root/anaconda3/bin/conda create -y python=3.7 --prefix=/root/anaconda3/envs/test2
+  /root/anaconda3/bin/conda create -y python=3.7 --prefix=/root/anaconda3/envs/test2
 #  echo "end"
 #  /root/anaconda3/bin/conda init
   source activate /root/anaconda3/envs/test2
