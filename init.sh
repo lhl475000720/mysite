1f04b199bd3ced0d7cf8aca54542d2fd52694eef
hllei@fitgreat.cn <hllei@fitgreat.cn>
2020-09-25 14:08:59 +0800

    Add User

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 cd /home/lei/app
 
-if [ ! -d "/home/lei/app/test2/" ] ; then
-  sudo mkdir test2 test2/conf test2/logs test2/www
-else
-  cd /home/lei/app/test2/www
-fi
+#if [ ! -d "/home/lei/app/test2/" ] ; then
+#  sudo mkdir test2 test2/conf test2/logs test2/www
+#else
+#  cd /home/lei/app/test2/www
+#fi
 
 sudo docker cp jenkins-test:/var/jenkins_home/workspace/test2/. /home/lei/app/test2/www
 
